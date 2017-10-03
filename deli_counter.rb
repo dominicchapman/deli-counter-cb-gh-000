6e5b84db8fcd8de670da8ff36dd78810d7dc7e07
Dominic Chapman <dominicchapman@me.com>
2017-10-03 23:11:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
-  katz_deli.each_with_index { |customer, i| puts "#{i+1}. #{customer}"}
+  katz_deli.each.with_index { |customer, i| puts "#{i+1}. #{customer}"}
 end
 
 def take_a_number(katz_deli, new_customer)
