33f8ea8c68932a8f3ea7083480bab9f9bb354fc8
Dominic Chapman <dominicchapman@me.com>
2017-10-03 23:03:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty."
+    puts "The line is empty."
   end
   puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
