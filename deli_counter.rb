8e45c553a821f394766c6134ebc6bd9b1a323f32
Dominic Chapman <dominicchapman@me.com>
2017-10-03 23:16:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,12 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-  current_line = "The line is currently:"
-  katz_deli.each_with_index do |customer, i|
-    current_line << " #{i+1}. #{customer}"
+    current_line = "The line is currently:"
+    katz_deli.each_with_index do |customer, i|
+      current_line << " #{i+1}. #{customer}"
+    end
+    puts current_line
   end
-  puts current_line
 end
 
 def take_a_number(katz_deli, new_customer)
